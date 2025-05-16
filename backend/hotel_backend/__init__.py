552da759d6558105f73be747bdd6e58c88353708
Superiom <lineaperezanthoan@gmail.com>
2025-05-15 23:43:17 -0400

    Initial commit

---
+++ backend/hotel_backend/__init__.py
@@ -0,0 +1,9 @@
+# This is a sample Python script
+
+def greet(name):
+    """This function greets the user by name."""
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    greet(name)
