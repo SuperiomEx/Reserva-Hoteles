0e74b28e5c6ce3555c48fb59f11651f417ade535
Anthoan de Jesus Linea Perez <166497873+SuperiomEx@users.noreply.github.com>
2025-10-18 03:25:55 -0400

    Agregar ruta para editar habitaciones
    - Anadir ruta editar/:id en habitaciones.routes.ts
    
    - Permitir navegacion a formulario de edicion
    
    - Corregir problema de redireccion al dashboard

--- frontend/src/app/routes/habitaciones.routes.ts
+++ frontend/src/app/routes/habitaciones.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { ListaComponent } from '../components/habitaciones/lista/lista.component';
+import { HabitacionesComponent } from '../components/habitaciones/habitaciones.component';
 import { FormComponent } from '../components/habitaciones/form/form.component';
 
 export const habitacionesRoutes: Routes = [
-  { path: '', component: ListaComponent },
+  { path: '', component: HabitacionesComponent },
   { path: 'crear', component: FormComponent },
   { path: ':id', component: FormComponent },
-  { path: ':id/editar', component: FormComponent }
+  { path: ':id/editar', component: FormComponent },
+  { path: 'editar/:id', component: FormComponent }
 ];
